e701d2251b5a0f4f601c8a66eea6e01a51e58a34
JanThorbecke <janth@xs4all.nl>
2019-06-13 05:10:55 -0500

    WS15

--- marchenko/demo/WS15/setup.sh
+++ marchenko/demo/WS15/setup.sh
@@ -1,6 +1,7 @@
 
 export CWPROOT=/home/users/jan/SeisUnix/
 export PATH=.:$CWPROOT/bin:/lus/scratch/$USER/OpenSource/bin:$PATH:
+alias lt='ls -lart'
 
 module swap PrgEnv-cray PrgEnv-intel
 module list
@@ -8,5 +9,6 @@ module list
 mkdir -p /lus/scratch/$USER
 
 cd /lus/scratch/$USER
+rsync -av /lus/scratch/jan/OpenSource .
 #git clone https://github.com/JanThorbecke/OpenSource.git
 
