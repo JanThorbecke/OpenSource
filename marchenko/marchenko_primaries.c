3db379dd55bae364d821998afb73d1e1658dd708
JanThorbecke <janth@xs4all.nl>
2019-07-18 11:37:47 +0200

    progress ala fdacrtmc

--- marchenko/marchenko_primaries.c
+++ marchenko/marchenko_primaries.c
@@ -629,11 +629,11 @@ int main (int argc, char **argv)
         /* To Do optional write intermediate RR results to file */
 
         if (verbose) {
-            if(!((iend-ii-istart)%perc)) fprintf(stderr,"\b\b\b\b%3d%%",ii*10/(iend-istart));
+            if(!((iend-ii-istart)%perc)) fprintf(stderr,"\b\b\b\b%3d%%",(ii-istart)*10/(iend-istart));
             if((ii-istart)==10)t4=wallclock_time();
             if((ii-istart)==50){
                 t4=(wallclock_time()-t4)*((iend-istart)/40.0);
-                fprintf(stderr,"\r    %s: Estimated total compute time = %.2fs.\n    %s: Progress: %3d%%",xargv[0],t4,xargv[0],ii/((iend-istart)/10));
+                fprintf(stderr,"\r    %s: Estimated total compute time = %.2fs.\n    %s: Progress: %3d%%",xargv[0],t4,xargv[0],(ii-istart)/((iend-istart)/10));
             }
             //t4=wallclock_time();
             tii=(t4-t1)*((float)(iend-istart)/(ii-istart+1.0))-(t4-t1);
