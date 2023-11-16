99fafe26d563c0de46b72f8885734abe70c92cbb
JanThorbecke <janthorbecke@gmail.com>
2023-11-16 07:56:43 +0100

    added simple 3D FFT test for MKL and FFTlib

--- FFTlib/test/mkl3d.c
+++ FFTlib/test/mkl3d.c
@@ -28,18 +28,14 @@ int main (int argc, char **argv)
     nt=8;
     nft=nt;
     nf=nft/2+1;
-    nkx=4;
+    nkx=3;
     nky=4;
     xorig=0;
     yorig=0;
     rdata        = (float *)calloc(nkx*nky*nft,sizeof(float));
     cdata        = (complex *)calloc(nkx*nky*nf,sizeof(complex));
-    rdata[0] = 1.0;
+    rdata[1] = 1.0;
 
-
-    dim_sizes[0] = nft; dim_sizes[1]=nkx; dim_sizes[2]=nky;
-    //strides_in[0] = 0; strides_in[1]=1; strides_in[2]=nft; strides_in[3]=nkx*nft;
-    //strides_out[0] = 0; strides_out[1]=1; strides_out[2]=nf; strides_out[3]=nkx*nf;
     dim_sizes[0] = nky; dim_sizes[1]=nkx; dim_sizes[2]=nft;
     strides_in[0] = 0; strides_in[1]=nkx*nft; strides_in[2]=nft; strides_in[3]=1;
     strides_out[0] = 0; strides_out[1]=nkx*nf; strides_out[2]=nf; strides_out[3]=1;
@@ -52,75 +48,79 @@ int main (int argc, char **argv)
 
     status = DftiCommitDescriptor(my_desc_handle);
 
-yxt2wkykx(rdata, cdata, nft, nkx, nky, nft, nkx, nky, xorig, yorig);
+    /* forward transform from libgenfft.a */
+    yxt2wkykx(rdata, cdata, nft, nkx, nky, nft, nkx, nky, xorig, yorig);
 
-fprintf(stderr,"*** FFTlib after forward transform [nf][nky][nkx] ***\n");
-for (it=0; it<nf; it++) {
-for (iy=0; iy<nky; iy++) {
-for (ix=0; ix<nkx; ix++) {
-	fprintf(stderr,"out[%d][%d][%d]=%e %e\n", it, iy, ix, cdata[it*nkx*nky+iy*nkx+ix].r, cdata[it*nkx*nky+iy*nkx+ix].i );
-}
-}
-}
-wkykx2yxt(cdata, rdata, nft, nkx, nky, nft, nkx, nky, xorig, yorig);
+    fprintf(stderr,"*** FFTlib after forward transform [nf][nky][nkx] ***\n");
+    for (it=0; it<nf; it++) {
+    for (iy=0; iy<nky; iy++) {
+    for (ix=0; ix<nkx; ix++) {
+	    fprintf(stderr,"out[%d][%d][%d]=%e %e\n", it, iy, ix, cdata[it*nkx*nky+iy*nkx+ix].r, cdata[it*nkx*nky+iy*nkx+ix].i );
+    }
+    }
+    }
 
-fprintf(stderr,"*** FFTlib after backward transform [nky][nkx][nt] ***\n");
-for (iy=0; iy<nky; iy++) {
-for (ix=0; ix<nkx; ix++) {
-for (it=0; it<nft; it++) {
-	fprintf(stderr,"out[%d][%d][%d]=%e \n", iy, ix, it, rdata[iy*nkx*nft+ix*nft+it] );
-}
-}
-}
+    /* backward transform from libgenfft.a */
+    wkykx2yxt(cdata, rdata, nft, nkx, nky, nft, nkx, nky, xorig, yorig);
 
-memset(&cdata[0].r, 0, sizeof(complex)*nkx*nky*nf);
-memset(&rdata[0], 0, sizeof(float)*nkx*nky*nft);
-rdata[0] = 1.0;
+    fprintf(stderr,"*** FFTlib after backward transform [nky][nkx][nt] ***\n");
+    for (iy=0; iy<nky; iy++) {
+    for (ix=0; ix<nkx; ix++) {
+    for (it=0; it<nft; it++) {
+	    fprintf(stderr,"out[%d][%d][%d]=%e \n", iy, ix, it, rdata[iy*nkx*nft+ix*nft+it] );
+    }
+    }
+    }
 
-status = DftiComputeForward(my_desc_handle, rdata, (MKL_Complex8 *)&cdata[0]);
+    memset(&cdata[0].r, 0, sizeof(complex)*nkx*nky*nf);
+    //memset(&rdata[0], 0, sizeof(float)*nkx*nky*nft);
+    //rdata[1] = 1.0;
 
-fprintf(stderr,"*** MKL after forward transform [nky][nkx][nf] ***\n");
-for (iy=0; iy<nky; iy++) {
-for (ix=0; ix<nkx; ix++) {
-for (it=0; it<nf; it++) {
-	fprintf(stderr,"out[%d][%d][%d]=%e %e\n", iy, ix, it, cdata[iy*nkx*nf+ix*nf+it].r, cdata[iy*nkx*nf+ix*nf+it].i );
-}
-}
-}
+    /* forward transform from MKL */
+    status = DftiComputeForward(my_desc_handle, rdata, (MKL_Complex8 *)&cdata[0]);
 
-/* tranpose to [nf][nky][nkx] */
-/* the data organisation of MKL is strange, also the signs will be different than in yxt2wkykx(time forward -1 x,y 1) */
-cdataT        = (complex *)calloc(nkx*nky*nf,sizeof(complex));
-fprintf(stderr,"*** MKL after forward transform tranposed to [nf][nky][nkx] ***\n");
-for (it=0; it<nf; it++) {
-for (iy=0; iy<nky; iy++) {
-for (ix=0; ix<nkx; ix++) {
-	cdataT[it*nkx*nky+iy*nkx+ix] = cdata[iy*nkx*nf+ix*nf+it];
-	fprintf(stderr,"out[%d][%d][%d]=%e %e\n", it, iy, ix, cdataT[it*nkx*nky+iy*nkx+ix].r, cdataT[it*nkx*nky+iy*nkx+ix].i );
-}
-}
-}
+    fprintf(stderr,"*** MKL after forward transform [nky][nkx][nf] ***\n");
+    for (iy=0; iy<nky; iy++) {
+    for (ix=0; ix<nkx; ix++) {
+    for (it=0; it<nf; it++) {
+	    fprintf(stderr,"out[%d][%d][%d]=%e %e\n", iy, ix, it, cdata[iy*nkx*nf+ix*nf+it].r, cdata[iy*nkx*nf+ix*nf+it].i );
+    }
+    }
+    }
 
-memset(&rdata[0], 0, sizeof(float)*nkx*nky*nft);
-strides_in[0] = 0; strides_in[1]=1; strides_in[2]=nf; strides_in[3]=nkx*nf;
+    /* tranpose to [nf][nky][nkx] */
+    /* the signs of the FFT kernels in MKL will be different than in yxt2wkykx(time forward -1 x,y 1) */
+    cdataT        = (complex *)calloc(nkx*nky*nf,sizeof(complex));
+    fprintf(stderr,"*** MKL after forward transform tranposed to [nf][nky][nkx] ***\n");
+    for (it=0; it<nf; it++) {
+    for (iy=0; iy<nky; iy++) {
+    for (ix=0; ix<nkx; ix++) {
+	    cdataT[it*nkx*nky+iy*nkx+ix] = cdata[iy*nkx*nf+ix*nf+it];
+	    fprintf(stderr,"out[%d][%d][%d]=%e %e\n", it, iy, ix, cdataT[it*nkx*nky+iy*nkx+ix].r, cdataT[it*nkx*nky+iy*nkx+ix].i );
+    }
+    }
+    }
+
+    /* backward transform from MKL */
+    strides_in[0] = 0; strides_in[1]=1; strides_in[2]=nf; strides_in[3]=nkx*nf;
     strides_out[0] = 0; strides_out[1]=nkx*nft; strides_out[2]=nft; strides_out[3]=1;
     strides_in[0] = 0; strides_in[1]=nkx*nf; strides_in[2]=nf; strides_in[3]=1;
     status = DftiSetValue(my_desc_handle, DFTI_INPUT_STRIDES, strides_in);
     status = DftiSetValue(my_desc_handle, DFTI_OUTPUT_STRIDES, strides_out);
     status = DftiCommitDescriptor(my_desc_handle);
-status = DftiComputeBackward(my_desc_handle, (MKL_Complex8 *)&cdata[0], rdata);
+    status = DftiComputeBackward(my_desc_handle, (MKL_Complex8 *)&cdata[0], rdata);
 
-fprintf(stderr,"*** MKL after backward transform [nky][nkx][nt] ***\n");
-for (iy=0; iy<nky; iy++) {
-for (ix=0; ix<nkx; ix++) {
-for (it=0; it<nft; it++) {
-	fprintf(stderr,"out[%d][%d][%d]=%e \n", iy, ix, it, rdata[iy*nkx*nft+ix*nft+it] );
-}
-}
-}
-
-status = DftiFreeDescriptor(&my_desc_handle);
-
-return 0;
+    fprintf(stderr,"*** MKL after backward transform [nky][nkx][nt] ***\n");
+    for (iy=0; iy<nky; iy++) {
+    for (ix=0; ix<nkx; ix++) {
+    for (it=0; it<nft; it++) {
+	    fprintf(stderr,"out[%d][%d][%d]=%e \n", iy, ix, it, rdata[iy*nkx*nft+ix*nft+it] );
+    }
+    }
+    }
+    
+    status = DftiFreeDescriptor(&my_desc_handle);
+    
+    return 0;
 }
 
